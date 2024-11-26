1d2fc8a919a724b7ecf3bae322eae6c6887a9e0e
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2024-11-26 13:44:38 +0500

    Connecting to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true, // Make ConfigService globally available
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Import ConfigModule to access ConfigService
+      inject: [ConfigService], // Inject ConfigService
+      useFactory: async (configService: ConfigService) =>
+        typeOrmConfig(configService), // Pass ConfigService to the factory
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
